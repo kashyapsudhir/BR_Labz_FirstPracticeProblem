c2f39f7c43abf736e2beba0d3ad2332ebe48fa98
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-05-18 11:36:25 +0530

    [Class and method and class variable and parameter pass in method use in this UC8 ]

--- EmployeeWageProblems/EmployeeCheck.cs
+++ EmployeeWageProblems/EmployeeCheck.cs
@@ -9,16 +9,26 @@ namespace EmployeeWageProblems
     public class EmployeeCheck
     {
 
-        //Class variable declare here according to UC7
+        //Class variable declare here according to UC8
 
-        public const int IS_PART_TIME = 1;
+        public const int IS_PART_TIME = 1;  //it is constant variable beacause fullTime and Part Time is in all company
         public const int IS_FULL_TIME = 2;
-        public const int EMP_PER_RATE_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 5;
-        public const int MAX_HRS_IN_MONTH = 10;
-        public static void EmployeeAttendance() //method here acording to UC7
+
+
+        public int EMP_PER_RATE_HOUR;
+        public int NUM_OF_WORKING_DAYS;
+        public int MAX_HRS_IN_MONTH;
+        public void EmployeeAttendance(int emp_per_rate_hour,int num_of_working_days, int max_hrs_in_month) 
+            //method here and parameter pass 
         {
-            
+
+            //it is initalize when will be argument pass in method
+            this.EMP_PER_RATE_HOUR = emp_per_rate_hour;
+            this.NUM_OF_WORKING_DAYS = num_of_working_days;
+            this.MAX_HRS_IN_MONTH = max_hrs_in_month;
+
+
+
             int empHour = 0;
             int totalEmpHour = 0;
             int totalWorkingDays = 0;
--- EmployeeWageProblems/Program.cs
+++ EmployeeWageProblems/Program.cs
@@ -10,7 +10,17 @@ namespace EmployeeWageComputation
             Console.WriteLine("Welcome to Employee Wage Computation day 8 using class and method ");
 
 
-            EmployeeCheck.EmployeeAttendance();
+            EmployeeCheck employeeCheck = new EmployeeCheck();
+            Console.WriteLine("enter employee per rate hour of your company");
+            int emp_per_rate_hour_of_compny = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter number of working days of your company");
+            int num_of_working_days_of_company = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter maximum hours in month of as per as your company rule ");
+            int max_hrs_in_month_of_company = Convert.ToInt32(Console.ReadLine());
+
+            employeeCheck.EmployeeAttendance(emp_per_rate_hour_of_compny, num_of_working_days_of_company, max_hrs_in_month_of_company);
            
 
 
